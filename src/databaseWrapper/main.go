55b4033801092407275790a522c82e1fe6ace46f
gvtsstag <47695539+gvtsstag@users.noreply.github.com>
2022-05-01 13:54:33 -0700

    Update main.go

--- src/databaseWrapper/main.go
+++ src/databaseWrapper/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -9,26 +10,76 @@ import (
 var database string
 
 func main() {
-	fmt.Println("Hello Worlds")
+	arguments := os.Args[1:]
 
-	db := _establishDatabase("C:/Users/ststa/Documents/GitHub/finderDB/test.db")
-	defer db.Close()
+	fmt.Println("Welcome to Finder's database")
+	fmt.Printf("Arguments: %v\n", arguments)
 
-	addFile("C:/Users/ststa/Desktop/test.pdf")
-	getFiles()
+	for index, argument := range arguments {
 
+		switch argument {
+		//print help info
+		case "-h":
+			fmt.Println("finderDB is the database for the file/folder related plugins for Finder.")
+		//create the finderDB database
+		case "-create":
+			if len(arguments) < index+2 {
+				fmt.Println("Unable to create the database: no path or database name provided.")
+				return
+			}
+
+			if _, err := os.Lstat(arguments[index+1]); err != nil {
+				fmt.Println("Creating a database at " + arguments[index+1])
+				createDatabase(arguments[index+1])
+			}
+
+			database = arguments[index+1]
+		//add a file/folder to the database
+		case "-add":
+			if len(arguments) < index+2 {
+				fmt.Println("Unable to add the following file/folder: no path or file name provided.")
+			} else {
+				info, err := os.Lstat(arguments[index+1])
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				fmt.Println("Adding  " + info.Name())
+				addFile(arguments[index+1])
+			}
+
+		case "-find":
+			if len(arguments) < index+2 {
+				return
+			}
+
+			find(arguments[index+1])
+		} //switch argument
+	} //for argument in arguments
 }
 
-//export establishDatabase
-func establishDatabase(databaseName string) {
+//export createDatabase
+func createDatabase(databaseName string) {
 	database = databaseName
-	db := _establishDatabase(database)
+	db := establishDatabase(database)
 	defer db.Close()
 }
 
 //export addFile
 func addFile(pathToFile string) {
 	addToFileTable(database, pathToFile)
+}
+
+//export addDirectory
+func addDirectory(pathToDirectory string) {
+
+}
+
+//export find
+func find(query string) string {
+	findFile(database, query)
+
+	return ""
 }
 
 //export getFiles
